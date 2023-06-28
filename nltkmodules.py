2c537ea0d708e42b967c01601e2bb61f3e92092b
Gizmo-Verindipencil <gizmo.verindipencil.19920804@gmail.com>
2023-06-29 01:09:53 +0900

    Replace with a download implementation of the nltk module that looks better

--- nltkmodules.py
+++ nltkmodules.py
@@ -1,4 +1,14 @@
+# ref : https://github.com/delip/PyTorchNLPBook/issues/14#issuecomment-1057151683
+
 import nltk
+import ssl
 
 def load():
+    try:
+        _create_unverified_https_context = ssl._create_unverified_context
+    except AttributeError:
+        pass
+    else:
+        ssl._create_default_https_context = _create_unverified_https_context
+
     nltk.download("punkt")
